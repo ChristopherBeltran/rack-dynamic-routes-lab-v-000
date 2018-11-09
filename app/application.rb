562f989347a84e49fb81cac1c3fe89975c1ecd38
Christopher Beltran <christod.beltran@gmail.com>
2018-11-09 04:40:37 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,20 +8,24 @@ class Application
     
     
     if req.path.match(/items/)
-      search_item = req.path.split("/items/").last
-      if @@items.include?(search_item)
-        item = @@items.find{|i| i.name == search_item}
-        resp.write item.price
-      else
-        resp.write "Item not found"
-        resp.status = 400
-      end 
+      resp.status = 200
+      item_display
     else
       resp.write "Route not found"
       resp.status = 404
     end 
     resp.finish
   end 
+  
+  def item_display
+    search_item = req.path.split("/items/").last
+      if @@items.include?(search_item)
+        item = @@items.find{|i| i.name == search_item}
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
 end 
       
       
